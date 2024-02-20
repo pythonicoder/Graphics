01ac8e99bdfd9188b664269eb8a529c8a9823e1e
pythonicoder <sinankasikci6@gmail.com>
2024-02-21 01:40:21 +0300

    added graphics for Python

---
+++ README.md
@@ -0,0 +1,3 @@
+# Graphics in Python programing language
+
+This repository includes scripts for demostration purposes of graphics written in Python
---
+++ coronavirus.py
@@ -0,0 +1,14 @@
+from turtle import *
+color('maroon')
+bgcolor('black')  
+
+speed(8)
+
+b = 0
+
+while b < 200:
+    right(b)
+    
+    forward(b*2)
+    
+    b += 1
